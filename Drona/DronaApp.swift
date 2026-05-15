daea5295c9266ddd580e48d07fd019bc5107b2d2
Likhith <likhiththejasbgowda@gmail.com>
2026-05-15 22:57:12 +0530

    Initial Commit

---
+++ Drona/DronaApp.swift
@@ -0,0 +1,17 @@
+//
+//  DronaApp.swift
+//  Drona
+//
+//  Created by Likhith Thejas on 15/05/26.
+//
+
+import SwiftUI
+
+@main
+struct DronaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
